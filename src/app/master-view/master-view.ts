ae6268a5838c3fbe9ae397de43741fb9d79cc943
Claudio Pi <claudiopi@gmail.com>
2023-02-27 16:16:41 -0300

    Test App initial version

---
+++ src/app/master-view/master-view.ts
@@ -0,0 +1,35 @@
+import { html, css, LitElement } from 'lit';
+import { customElement } from 'lit/decorators.js';
+import { defineComponents, IgcButtonComponent, IgcRippleComponent } from 'igniteui-webcomponents';
+
+defineComponents(IgcButtonComponent, IgcRippleComponent);
+
+@customElement('app-master-view')
+export default class MasterView extends LitElement {
+  static styles = css`
+    :host {
+      height: 100%;
+      display: flex;
+      justify-content: flex-start;
+      align-items: stretch;
+      align-content: flex-start;
+    }
+    .button {
+      height: max-content;
+      min-width: min-content;
+      flex-shrink: 0;
+    }
+  `;
+
+  render() {
+    return html`
+      <link href='https://fonts.googleapis.com/icon?family=Material+Icons' rel='stylesheet'>
+      <link href='https://fonts.googleapis.com/css?family=Titillium+Web' rel='stylesheet'>
+      <link rel='stylesheet' href='../../ig-theme.css'>
+      <igc-button class="button">
+        Button
+        <igc-ripple></igc-ripple>
+      </igc-button>
+    `;
+  }
+}
